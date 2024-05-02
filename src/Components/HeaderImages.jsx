6433525c0487b9adfb3bfdd2d3bb09f02a58b7a0
AhmedAhmed00 <devahmedahmed@gmail.com>
2024-05-02 21:34:44 +0300

    bg-images

--- src/Components/HeaderImages.jsx
+++ src/Components/HeaderImages.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
 import SliderContainer from './SliderContainer'
-import sliderImage from "../../public/slider-2.jpeg"
+import sliderImage from "../../public/iphone.png"
+import sliderImage2 from "../../public/slider-2.jpeg"
+import sliderImage3 from "../../public/grocery-banner-2.jpeg"
+import sliderImage4 from "../../public/grocery-banner.png"
+// import sliderImage3 from "../../public/slider-2.jpeg"
+// import sliderImage4 from "../../public/slider-2.jpeg"
 
 export default function HeaderImages() {
     return (
@@ -8,13 +13,24 @@ export default function HeaderImages() {
 
             <SliderContainer slidesToShow={1} slidesToScroll={1} draggable={true} isResponsive={false} >
 
+
                 <div >
-                    <img src={sliderImage} alt="" className='rounded-xl xs:h-screen md:h-auto  object-cover' />
+                    <img src={sliderImage2} alt="" className='rounded-xl w-full  xs:h-screen md:h-auto object-cover ' />
                 </div>
 
                 <div >
-                    <img src={sliderImage} alt="" className='rounded-xl  xs:h-screen md:h-auto object-cover ' />
+                    <img src={sliderImage} alt="" className='rounded-xl w-full xs:h-screen md:h-auto  object-cover' />
                 </div>
+
+                <div >
+                    <img src={sliderImage3} alt="" className='rounded-xl w-full xs:h-screen md:h-auto  object-cover' />
+                </div>
+                <div >
+                    <img src={sliderImage4} alt="" className='rounded-xl w-full xs:h-screen md:h-auto  object-cover' />
+                </div>
+
+
+
 
             </SliderContainer>
         </div>
